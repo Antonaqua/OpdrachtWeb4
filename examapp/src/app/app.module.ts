ab51e754b9b0d5cb48651b2860fd86486d96edbd
anton engels <anton.engels.w0190@student.hogent.be>
2018-05-02 15:28:42 +0200

    Generated Component 'Deck'

--- examapp/src/app/app.module.ts
+++ examapp/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DeckComponent } from './deck/deck.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeckComponent
   ],
   imports: [
     BrowserModule
