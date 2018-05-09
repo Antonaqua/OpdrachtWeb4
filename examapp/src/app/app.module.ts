6c5f55015d091426222282444fbd6e73c7a39f68
anton engels <anton.engels.w0190@student.hogent.be>
2018-05-09 15:48:20 +0200

    Created AddDeck Component

--- examapp/src/app/app.module.ts
+++ examapp/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DeckComponent } from './deck/deck.component';
 import { CardComponent } from './card/card.component';
+import { AddDeckComponent } from './add-deck/add-deck.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DeckComponent,
-    CardComponent
+    CardComponent,
+    AddDeckComponent
   ],
   imports: [
     BrowserModule
